bdd92ac825ff9c3ef8dce6ff73bd06bd062efdef
Scott Berenzweig <scott@betterbrand.com>
2023-07-16 11:49:49 -0700

    cleaned readme, blank page txt and header

--- components/header.tsx
+++ components/header.tsx
@@ -53,7 +53,8 @@ export async function Header() {
         </div>
       </div>
       <div className="flex items-center justify-end space-x-2">
-        <a
+
+        {/* <a
           target="_blank"
           href="https://github.com/vercel/nextjs-ai-chatbot/"
           rel="noopener noreferrer"
@@ -61,8 +62,9 @@ export async function Header() {
         >
           <IconGitHub />
           <span className="hidden ml-2 md:flex">GitHub</span>
-        </a>
-        <a
+        </a> */}
+
+        {/*<a
           href="https://github.com/vercel/nextjs-ai-chatbot/"
           target="_blank"
           className={cn(buttonVariants())}
@@ -70,7 +72,8 @@ export async function Header() {
           <IconVercel className="mr-2" />
           <span className="hidden sm:block">Deploy to Vercel</span>
           <span className="sm:hidden">Deploy</span>
-        </a>
+        </a> 
+        */}
       </div>
     </header>
   )
